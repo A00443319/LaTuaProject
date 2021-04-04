7db789259d66cbea9bda575e0cd5c5851a52e0b8
dipeshtimilsina <dipesh.nepali@gmail.com>
2021-04-04 01:02:51 -0300

    Added signup model

---
+++ LaTuaPizza/Models/Signup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaTuaPizza.Models
+{
+    public class Signup
+    {
+        public Signup()
+        {
+            Address = new HashSet<Address>();
+            CardDetails = new HashSet<CardDetails>();
+            OrderInfo = new HashSet<OrderInfo>();
+        }
+
+        public int Phone { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Email { get; set; }
+
+        public virtual LoginCred EmailNavigation { get; set; }
+        public virtual ICollection<Address> Address { get; set; }
+        public virtual ICollection<CardDetails> CardDetails { get; set; }
+        public virtual ICollection<OrderInfo> OrderInfo { get; set; }
+    }
+}
